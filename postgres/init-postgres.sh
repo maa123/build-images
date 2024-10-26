8bc7c17cbd152bdd4e1f0e5f6240b80f05dce143
maa123 <maa123.lq@gmail.com>
2024-10-26 09:44:42 +0900

    fix script

--- postgres/init-postgres.sh
+++ postgres/init-postgres.sh
@@ -3,7 +3,5 @@
 # データベースが初期化済みかつPOSTGRES_USER, POSTGRES_PASSWORDが設定されている場合
 if [ -f "$PGDATA/PG_VERSION" ] && [ -n "$POSTGRESQL_USER" ] && [ -n "$POSTGRESQL_PASSWORD" ]; then
     # パスワードを更新
-    su postgres -c "psql -c \"ALTER USER $POSTGRESQL_USER WITH PASSWORD '$POSTGRESQL_PASSWORD';\""
+    postgres -c "psql -c \"ALTER USER $POSTGRESQL_USER WITH PASSWORD '$POSTGRESQL_PASSWORD';\""
 fi
-
-postgres
