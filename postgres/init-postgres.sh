12bc98ab2b1b02fc5b75364cb0a665d2b5e77d3d
maa123 <maa123.lq@gmail.com>
2024-10-26 09:01:16 +0900

    add postgres

---
+++ postgres/init-postgres.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# データベースが初期化済みかつPOSTGRES_USER, POSTGRES_PASSWORDが設定されている場合
+if [ -f "$PGDATA/PG_VERSION" ] && [ -n "$POSTGRESQL_USER" ] && [ -n "$POSTGRESQL_PASSWORD" ]; then
+    # パスワードを更新
+    su postgres -c "psql -c \"ALTER USER $POSTGRESQL_USER WITH PASSWORD '$POSTGRESQL_PASSWORD';\""
+fi
+
+exec postgres
